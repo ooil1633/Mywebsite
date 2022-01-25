1f058fc0fbaf1e49171d8b2c41b606852d3574b7
ooil1633 <97937652+ooil1633@users.noreply.github.com>
2022-01-25 13:42:05 +0700

    Update page1 .php

--- page1 .php
+++ page1 .php
@@ -1,7 +1,10 @@
 <html>
         <head>
+                <title>งานที่ 1</title>
         </head>
-        <body bgcolor="a60400">
-                page1
+        <body bgcolor="710016">
+                page1ธรรมดา
+                <br>
+                <b>page1 หนา</b>
         </body>
 </html>
