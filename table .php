d24646c80a408226471906c1ebfd7070f60b0eb3
ooil1633 <97937652+ooil1633@users.noreply.github.com>
2022-02-08 14:58:41 +0700

    Update table .php

--- table .php
+++ table .php
@@ -18,6 +18,40 @@
                                 <td><img src="https://static.trueplookpanya.com/tppy/member/m_545000_547500/545994/cms/images/2018-04-Apr/namnueng_14.jpg" width="130px"></td>
                                 <td><a href="https://twitter.com/bnk48official">twitter</a></td>
                         </tr>
-                        
+                        <tr>
+                                <td>2</td>
+                                <td>กานต์ธีรา วัชรทัศนกุล</td>
+                                <td>เนย</td>
+                                <td><img src="https://www.thebangkokinsight.com/wp-content/uploads/2021/02/batch_113-1.jpg" width="130px"></td>
+                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>3</td>
+                                <td>พัศชนันท์ เจียจิรโชติ</td>
+                                <td>อร</td>
+                                <td><img src="https://www.siamzone.com/ig/media/4636727204/2663274736320250159-1.jpg" width="130px"></td>
+                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>4</td>
+                                <td>ณัฐทิชา จันทรวารีเลขา</td>
+                                <td>ฟ้อนต์</td>
+                                <td><img src="https://thailandsuperstar.com/images/pix/7613280384/T_2256080949653329982.jpg" width="130px"></td>
+                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>5</td>
+                                <td>ปัญสิกรณ์ ติยะกร</td>
+                                <td>ปัญ</td>
+                                <td><img src="https://static.trueplookpanya.com/tppy/member/m_545000_547500/545994/cms/images/2018-02-Feb/pun_05.jpg" width="130px"></td>
+                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>6</td>
+                                <td>เจนนิษฐ์ โอ่ประเสิรฐ</td>
+                                <td>เจนนิษฐ์</td>
+                                <td><img src="https://f.ptcdn.info/833/073/000/quqnns11ab27EjGGcDfp-o.jpg" width="130px"></td>
+                                <td><a href="https://twitter.com/bnk48official">twitter</a></td>
+                        </tr>
         </body>
 </html>
