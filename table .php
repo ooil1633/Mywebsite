7da7edd0e65b5c9b2e7361f029a4232bae5069f6
ooil1633 <97937652+ooil1633@users.noreply.github.com>
2022-02-08 14:28:20 +0700

    Update table .php

--- table .php
+++ table .php
@@ -9,7 +9,7 @@
                     <th>two</th>
                   </tr>
                   <tr>
-                    <td>one</td>
+                    <td><img.src="IMG_20210105_175435"</td>
                     <td>two</td>
                   </tr>
                 </table>
