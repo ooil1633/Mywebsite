cd911c50fcb66f8b5ba8c20328fe836915191f1a
ooil1633 <97937652+ooil1633@users.noreply.github.com>
2022-02-08 14:46:57 +0700

    Update table .php

--- table .php
+++ table .php
@@ -15,7 +15,7 @@
                                 <td>1</td>
                                 <td>มิลิน ดอกเทียน</td>
                                 <td>น้ำหนึ่ง</td>
-                                <td><img src="https://static.trueplookpanya.com/tppy/member/m_545000_547500/545994/cms/images/2018-04-Apr/namnueng_14.jpeg" width="130px"></td>
+                                <td><img src="https://static.trueplookpanya.com/tppy/member/m_545000_547500/545994/cms/images/2018-04-Apr/namnueng_14.jpg" width="130px"></td>
                                 <td><a href="https://twitter.com/bnk48official">twitter</a></td>
                         </tr>
                         
