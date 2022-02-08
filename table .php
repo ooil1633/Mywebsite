1579ccf548df0e2dfc1b300d8a30332d6c145528
ooil1633 <97937652+ooil1633@users.noreply.github.com>
2022-02-08 14:19:05 +0700

    Update table .php

--- table .php
+++ table .php
@@ -3,10 +3,11 @@
                 <title>งานที่ 3</title>
         </head>
         <body bgcolor="7cffea">
-                <table>
+                <table border="1">
                   <tr>
                     <td>one</td>
                     <td>two</td>
                   </tr>
+                </table>
         </body>
 </html>
