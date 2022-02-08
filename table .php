56449c8c9bb1be8f4c855855df694b6678141bd5
ooil1633 <97937652+ooil1633@users.noreply.github.com>
2022-02-08 14:16:22 +0700

    Update table .php

--- table .php
+++ table .php
@@ -1,1 +1,12 @@
-
+<html>
+        <head>
+                <title>งานที่ 3</title>
+        </head>
+        <body bgcolor="7cffea">
+                <table>
+                  <tr>
+                    <td>one</td>
+                    <td>two</td>
+                  </tr>
+        </body>
+</html>
