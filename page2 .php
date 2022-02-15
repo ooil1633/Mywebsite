2f2de28848d0ea6c56b7eef0e9cdd46f78d99fa3
ooil1633 <97937652+ooil1633@users.noreply.github.com>
2022-02-15 13:45:39 +0700

    Update page2 .php

--- page2 .php
+++ page2 .php
@@ -2,20 +2,16 @@
 	<head>
 		<title> css </title>
 		<style>
-			body{
-				background-color: fbe8f1;
-			}
 			#text1{
-				color: ff4b6b;
-			}
-			
-			#text2{
-				color: b0ffff;
-			}
+				width= 800px;
+				height= 380px;
+				background-color= ffd3db;
+			}	
 		</style>
 	</head>
 	<body>
-		<h1 id="text1"> namneungnoey </h1>
-		<h1 id="text2"> pfg </h1>
+		<div id="text1">
+			<h1> namneungnoey </h1>
+		</div>
 	</body>
 </html>
