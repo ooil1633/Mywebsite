bd24f05ff54f7a18427b7aac13e64c9cfd126513
ooil1633 <97937652+ooil1633@users.noreply.github.com>
2022-02-15 13:16:21 +0700

    Update page2 .php

--- page2 .php
+++ page2 .php
@@ -1,9 +1,17 @@
 <html>
 	<head>
 		<title> css </title>
+		<style>
+			h1{
+				color: b0ffff;
+			}
+			
+			h2{
+				color: b0ffff;
+			}
 	</head>
 	<body>
-		<p style="color:blue;"> eiei </p>
-    
+		<h1> eiei </h1>
+		<h2> eiei </h2>
 	</body>
 </html>
