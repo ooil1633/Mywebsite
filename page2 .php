2973cd03de580720b7ead7a8cc614f7d4b90bc0c
ooil1633 <97937652+ooil1633@users.noreply.github.com>
2022-02-15 13:56:35 +0700

    Update page2 .php

--- page2 .php
+++ page2 .php
@@ -2,25 +2,20 @@
 	<head>
 		<title> css </title>
 		<style>
-			.nnn{
-				background-color: ffd3db;
+			body{
+				background-color: ffa0ff;
 			}
 			#box1{
 				width: 800px;
 				height: 380px;
-			}
-			#box2{
-				width: 600px;
-				height: 380px;
+				color: ff5;
+				background-color: c8ecdc;
 			}
 		</style>
 	</head>
 	<body>
-		<div class="nnn" id="box1">
-			<h1> namneungnoey </h1>
-		</div>
-		<div class="nnn" id="box2">
-			<h1> namneungnoey </h1>
+		<div id="box1">
+			<h1> natnicha khotsa 27 4/13 </h1>
 		</div>
 	</body>
 </html>
