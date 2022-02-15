427ed984d5e1709f20bf82580ac1c80b35cb9100
ooil1633 <97937652+ooil1633@users.noreply.github.com>
2022-02-15 13:33:35 +0700

    Update page2 .php

--- page2 .php
+++ page2 .php
@@ -12,13 +12,10 @@
 			#text2{
 				color: b0ffff;
 			}
-			#text3{
-				color: b0ffff;
-			}
 		</style>
 	</head>
 	<body>
-		<h1 id="text1"> namneung </h1><h1 id="text2"> noey </h1>
-		<h1 id="text3"> pfg </h1>
+		<h1 id="text1"> namneungnoey </h1>
+		<h1 id="text2"> pfg </h1>
 	</body>
 </html>
